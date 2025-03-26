92242958616f54ec58cde61cda915f4a6f163f16
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-26 10:14:45 +0530

    [Pranav] ADD compare-sorting-algorithms-complexities

---
+++ CompareSortingAlgorithms.java
@@ -0,0 +1,110 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class CompareSortingAlgorithms {
+
+    // Bubble Sort
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        boolean swapped;
+        for (int i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break;
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, mid + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int N = 10000;
+        Random rand = new Random();
+        int[] originalArray = rand.ints(N, 1, 100000).toArray();
+
+        int[] arr1 = Arrays.copyOf(originalArray, originalArray.length);
+        int[] arr2 = Arrays.copyOf(originalArray, originalArray.length);
+        int[] arr3 = Arrays.copyOf(originalArray, originalArray.length);
+
+        long start, end;
+
+        // Bubble Sort
+        start = System.nanoTime();
+        bubbleSort(arr1);
+        end = System.nanoTime();
+        System.out.println("Bubble Sort took " + (end - start) / 1000000.0 + " ms");
+
+        // Merge Sort
+        start = System.nanoTime();
+        mergeSort(arr2, 0, arr2.length - 1);
+        end = System.nanoTime();
+        System.out.println("Merge Sort took " + (end - start) / 1000000.0 + " ms");
+
+        // Quick Sort
+        start = System.nanoTime();
+        quickSort(arr3, 0, arr3.length - 1);
+        end = System.nanoTime();
+        System.out.println("Quick Sort took " + (end - start) / 1000000.0 + " ms");
+    }
+}
